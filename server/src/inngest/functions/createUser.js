b2b037e8f8d358895d867a5c3ad65efa67becf73
its-bismay <2023ugee070@nitjsr.ac.in>
2026-04-10 19:16:44 +0530

    feat: implement Inngest function for Clerk user synchronization and add environment configuration

--- server/src/inngest/functions/createUser.js
+++ server/src/inngest/functions/createUser.js
@@ -1,9 +1,11 @@
 import User from "../../models/user.model.js";
 import { inngest } from "../client.js";
+import { connectDB } from "../../lib/db.js";
 
 export const createUser = inngest.createFunction(
   { id: "create-user-from-clerk", triggers: [{ event: "clerk/user.created" }] },
   async ({ event }) => {
+    await connectDB(); // ensure connection before any DB ops
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
     const email = email_addresses[0]?.email_address;
